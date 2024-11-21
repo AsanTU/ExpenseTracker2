ee2736de08391ce89740d37db3731bc110a79d86
AsanTU <asantursunaliev0606@gmail.com>
2024-11-21 16:30:06 +0600

    First commit

---
+++ app/src/main/java/com/example/expensetracker2/LoginScreenFragment.kt
@@ -0,0 +1,36 @@
+package com.example.expensetracker2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.expensetracker2.databinding.FragmentListOfExpensesBinding
+import com.example.expensetracker2.databinding.FragmentLoginScreenBinding
+
+class LoginScreenFragment : Fragment() {
+
+    private var _binding: FragmentLoginScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLoginScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginScreenFragment_to_listOfExpensesFragment2)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/expensetracker2/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.expensetracker2
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setSystemUIFlags()
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setSystemUIFlags()
+    }
+
+    private fun setSystemUIFlags() {
+        val backgroundColor = Color.parseColor("#2C2C2C")
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+
+        window.statusBarColor = backgroundColor
+        window.navigationBarColor = backgroundColor
+    }
+}
