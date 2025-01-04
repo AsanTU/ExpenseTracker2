cc5b7e232ff2715dfbcf0b5f03afb9a32d3e6425
Emir <148708322+eeemmm29@users.noreply.github.com>
2025-01-04 15:54:15 +0600

    Make Toasts independent of fragments

--- app/src/main/java/com/example/expensetracker2/ui/fragments/LoginScreenFragment.kt
+++ app/src/main/java/com/example/expensetracker2/ui/fragments/LoginScreenFragment.kt
@@ -8,18 +8,18 @@ import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
-import com.example.expensetracker2.utils.ApiServiceHelper
 import com.example.expensetracker2.R
-import com.example.expensetracker2.utils.RetrofitClient
 import com.example.expensetracker2.databinding.FragmentLoginScreenBinding
 import com.example.expensetracker2.models.LoginRequest
 import com.example.expensetracker2.models.LoginResponse
+import com.example.expensetracker2.utils.ApiServiceHelper
+import com.example.expensetracker2.utils.RetrofitClient
 import com.example.expensetracker2.utils.SharedPreferencesManager
+import com.example.expensetracker2.utils.Utils
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -86,9 +86,9 @@ class LoginScreenFragment : Fragment() {
             val email = binding.emailEt.text.toString().trim()
             val password = binding.passwordEt.text.toString().trim()
             if (email.isEmpty() || password.isEmpty()) {
-                Toast.makeText(requireContext().applicationContext, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+                Utils.showToastMessage(requireContext().applicationContext, "Please fill in all fields")
             } else if (!isValidEmail(email)) {
-                Toast.makeText(requireContext().applicationContext, "Please enter a valid email address", Toast.LENGTH_SHORT).show()
+                Utils.showToastMessage(requireContext().applicationContext, "Please enter a valid email address")
             } else {
                 isLoginInProgress = true
                 loginUser(email, password)
@@ -111,17 +111,17 @@ class LoginScreenFragment : Fragment() {
                         loginResponse.accessTokenExpiresAt?.let { SharedPreferencesManager.storeAccessTokenExpiresAt(it) }
                         loginResponse.refreshTokenExpiresAt?.let { SharedPreferencesManager.storeRefreshTokenExpiresAt(it) }
 
-                        Toast.makeText(requireContext().applicationContext, "Logged in successfully!", Toast.LENGTH_SHORT).show() // TODO: Make Toasts independent of fragments
+                        Utils.showToastMessage(requireContext().applicationContext, "Logged in successfully!")
                         navigateToExpenseList()
                     } else {
                         val errorMessage = loginResponse?.message ?: "Login failed"
                         Log.d("LoginError", errorMessage)
-                        Toast.makeText(requireContext().applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+                        Utils.showToastMessage(requireContext().applicationContext, errorMessage)
                     }
                 } else {
                     val errorMessage = ApiServiceHelper.getErrorMessage(response, "Failed to log in.")
                     Log.d("LoginError", errorMessage)
-                    Toast.makeText(requireContext().applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+                    Utils.showToastMessage(requireContext().applicationContext, errorMessage)
                 }
                 isLoginInProgress = false // Reset flag here
             }
@@ -129,7 +129,7 @@ class LoginScreenFragment : Fragment() {
             override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
                 val errorMessage = "Error: ${t.message}"
                 Log.d("LoginError", errorMessage)
-                Toast.makeText(requireContext().applicationContext, errorMessage, Toast.LENGTH_SHORT).show()
+                Utils.showToastMessage(requireContext().applicationContext, errorMessage)
                 isLoginInProgress = false // Reset flag here too
             }
         })
